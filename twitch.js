03484436dad750001e0cf44101daf93fcd6e3af7
Zach Fudge <zacheryfudge@gmail.com>
2018-01-23 21:32:43 -0700

    added httprequest

--- twitch.js
+++ twitch.js
@@ -2,3 +2,16 @@ function checkBox(input) {
   Array.from(document.getElementsByTagName('input')).forEach(cur => cur.checked = false);
   input.checked = true;
 }
+
+function requestChannelData(channel) {
+  const xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      console.log(xhr.response);
+    }
+  }
+  xhr.open("GET", `https://wind-bow.gomix.me/twitch-api/users/${channel}?origin=*`);
+  xhr.send();
+}
+
+requestChannelData('freecodecamp');
