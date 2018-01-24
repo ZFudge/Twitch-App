164dd056c31a59af69bd4fe060f1bdd563e28151
Zach Fudge <zacheryfudge@gmail.com>
2018-01-24 09:57:35 -0700

    receiving json response from api, appending results to unordered list

--- twitch.js
+++ twitch.js
@@ -3,15 +3,64 @@ function checkBox(input) {
   input.checked = true;
 }
 
-function requestChannelData(channel) {
+const twitch = {
+  list: document.getElementById('list'),
+  users: ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"],
+  responses: []
+};
+
+function requestChannelData(channel,index) {
   const xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function() {
     if (xhr.readyState === XMLHttpRequest.DONE) {
-      console.log(xhr.response);
+      const response = JSON.parse(xhr.response);
+      twitch.responses.push(response);
+      const li = document.createElement('li');
+      const img = document.createElement('img');
+      img.src = response.logo;
+      const span = document.createElement('span');
+      const a = document.createElement('a');
+      a.href = `https://www.twitch.tv/${response.display_name}`;
+      a.innerHTML = response.display_name;
+      a.target = '_blank';
+      span.appendChild(a);
+      li.appendChild(img);
+      li.appendChild(span);
+      twitch.list.appendChild(li);
+      li.style.opacity = 1;
     }
   }
-  xhr.open("GET", `https://wind-bow.gomix.me/twitch-api/users/${channel}?origin=*`);
+  xhr.open("GET", `https://wind-bow.glitch.me/twitch-api/users/${channel}`);
   xhr.send();
 }
 
-requestChannelData('freecodecamp');
+twitch.response
+twitch.users.forEach(function(cur,ind) {
+  requestChannelData(cur,ind);
+});
+
+//setTimeout(function(){
+//  Array.from(twitch.list.children).forEach(cur=>cur.style.opacity = 1)
+//},1000);
+
+/*
+
+setTimeout(function() {
+  twitch.responses.forEach(function(cur) {
+    const li = document.createElement('li');
+    const img = document.createElement('img');
+    img.src = cur.logo;
+    const span = document.createElement('span');
+    const a = document.createElement('a');
+    a.href = `https://www.twitch.tv/${cur.display_name}`;
+    a.innerHTML = cur.display_name;
+    a.target = '_blank';
+    span.appendChild(a);
+    li.appendChild(img);
+    li.appendChild(span);
+    twitch.list.appendChild(li);
+  });
+
+},0);
+
+*/
