0e7b25a58fbc83abae379dccc036063fbb1a1f98
Zach Fudge <zacheryfudge@gmail.com>
2018-01-24 13:46:27 -0700

    reduced arguments from requestTwitchData, functions refactored as methods of twitch object

--- twitch.js
+++ twitch.js
@@ -6,49 +6,48 @@ function checkBox(input) {
 const twitch = {
   list: document.getElementById('list'),
   users: ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"],
-  responses: []
+  requestTwitchData: function(path,streamer,fn) {
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === XMLHttpRequest.DONE) {
+        const response = JSON.parse(xhr.response);
+        fn(response);
+      }
+    }
+    xhr.open("GET", `https://wind-bow.glitch.me/twitch-api/${path}/${streamer}`);
+    xhr.send();
+  },
+  handleStreams: function(response, path, status) {
+    const li = document.createElement('li');
+
+    const img = document.createElement('img');
+    img.src = path.logo;
+
+    const nameSpan = document.createElement('span');
+
+    const a = document.createElement('a');
+    a.href = path.url;
+    a.innerHTML = path.display_name;
+    a.target = '_blank';
+    nameSpan.appendChild(a);
+
+    const statusSpan = document.createElement('span');
+    const p = document.createElement('p');
+    p.innerHTML = status;
+    statusSpan.appendChild(p);
+
+    li.appendChild(img);
+    li.appendChild(nameSpan);
+    li.appendChild(statusSpan);
+    twitch.list.appendChild(li);
+    li.style.opacity = 1;
+  }
 };
 
-function requestTwitchData(path,streamer,fn) {
-  const xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState === XMLHttpRequest.DONE) {
-      const response = JSON.parse(xhr.response);
-      fn(response);
-    }
-  }
-  xhr.open("GET", `https://wind-bow.glitch.me/twitch-api/${path}/${streamer}`);
-  xhr.send();
-}
-
 twitch.users.forEach(function(cur) {
-  requestTwitchData('streams', cur, function(firstResponse) {
-    (firstResponse.stream === null) ? requestTwitchData('channels', cur, function(secondResponse) {
-      handleStreams(secondResponse, secondResponse.logo, secondResponse.display_name, "Offline");
-    }) : handleStreams(firstResponse, firstResponse.stream.channel.logo, firstResponse.stream.channel.display_name, `${firstResponse.stream.channel.game}\: ${firstResponse.stream.channel.status}`);
+  twitch.requestTwitchData('streams', cur, function(firstResponse) {
+    (firstResponse.stream === null) ? twitch.requestTwitchData('channels', cur, function(secondResponse) {
+      twitch.handleStreams(secondResponse, secondResponse, "Offline");
+    }) : twitch.handleStreams(firstResponse, firstResponse.stream.channel, `${firstResponse.stream.channel.game}\: ${firstResponse.stream.channel.status}`);
   });
 });
-
-function handleStreams(response, picture, name, status) {
-  twitch.responses.push(response);
-  const li = document.createElement('li');
-
-  const img = document.createElement('img');
-  img.src = picture;
-
-  const nameSpan = document.createElement('span');
-  const a = document.createElement('a');
-  a.href = response.url;
-  a.innerHTML = name;
-  a.target = '_blank';
-  nameSpan.appendChild(a);
-
-  const statusSpan = document.createElement('span');
-  statusSpan.innerHTML = status;
-
-  li.appendChild(img);
-  li.appendChild(nameSpan);
-  li.appendChild(statusSpan);
-  twitch.list.appendChild(li);
-  li.style.opacity = 1;
-}
